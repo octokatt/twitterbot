7973a513c7f066f218f5fb7a53f41c59af95cecc
octokatt <katt@fatehold.com>
2021-11-18 16:28:21 -0800

    quote puller done, now working on shuffle

--- api_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-response = requests.get("https://api.weather.gov/points/45.6763,-122.5416")
-
-results = response.json()
-
-print(response)
-print(results)
--- quoter.py
+++ quoter.py
@@ -1,12 +1,33 @@
-import csv
 import pandas as pd
 
-with open('quotes.csv') as quotes_csv:
-  quotes = csv.reader(quotes_csv, dialect='excel')
-  header = next(quotes)
-  quote = next(quotes)
+def get_quote():
+  # Pull a quote from the csv
+  quotes = pd.read_csv('quotes.csv')
 
-  print(quote)
+  my_quote = quotes.iloc[0]
+  quote = my_quote[0]
+  author = my_quote[1]
+
+  # # Shuffle the csv and put it back
+  # quotes = quotes.drop(quotes.index[0])
+  # quotes.iloc[-1] = my_quote
+  # quotes.reset_index()
+  # quotes.to_csv('quotes.csv')
+
+  return quote, author
 
 
-# print(quote)
+def create_tweet(quote, author):
+  return "{}  -- {}".format(quote, author)
+
+# tweet = create_tweet(get_quote())
+
+def assemble():
+  quote, author = get_quote()
+  tweet = create_tweet(quote, author)
+  return tweet
+
+sample = assemble()
+print(sample)
+
+
