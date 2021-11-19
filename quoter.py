d04bc8a0b478e78ea9ce44dbef36fd9e0780d36a
octokatt <katt@fatehold.com>
2021-11-18 17:31:12 -0800

    things are working?

--- quoter.py
+++ quoter.py
@@ -1,33 +1,36 @@
 import pandas as pd
 
-def get_quote():
-  # Pull a quote from the csv
+def pull_quotes():
   quotes = pd.read_csv('quotes.csv')
+  return quotes
+
+def get_quote():
+  quotes = pull_quotes()
 
   my_quote = quotes.iloc[0]
   quote = my_quote[0]
   author = my_quote[1]
 
-  # # Shuffle the csv and put it back
-  # quotes = quotes.drop(quotes.index[0])
-  # quotes.iloc[-1] = my_quote
-  # quotes.reset_index()
-  # quotes.to_csv('quotes.csv')
-
   return quote, author
-
 
 def create_tweet(quote, author):
   return "{}  -- {}".format(quote, author)
-
-# tweet = create_tweet(get_quote())
 
 def assemble():
   quote, author = get_quote()
   tweet = create_tweet(quote, author)
   return tweet
 
-sample = assemble()
-print(sample)
 
+def shuffle_quotes(quotes):
+  shuffled = quotes.iloc[1:]
+  shuffled.append(quotes.iloc[0])
+  shuffled.reset_index()
+  return shuffled
 
+def update_quotes(new_file):
+  new_file.to_csv('quotes_shuffed.csv', index=False)
+
+def update_quotes_file():
+  new_csv = shuffle_quotes()
+  update_quotes(new_csv)
